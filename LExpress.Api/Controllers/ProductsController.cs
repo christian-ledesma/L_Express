ebb6170389710ad2d4e6d61b480865aeb7a72a48
Christian Ledesma <christian.ledesma@neoris.com>
2022-05-30 17:01:30 +0200

    Add API project and add entity

---
+++ LExpress.Api/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LExpress.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this is a list of products";
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public string GetProduct(int id)
+        {
+            return "this is a single products";
+        }
+    }
+}
---
+++ LExpress.Api/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using LExpress.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LExpress.Api.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
