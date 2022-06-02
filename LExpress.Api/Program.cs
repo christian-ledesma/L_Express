e93e92aa26eb37ae079ef2e4269d0219fdd336f4
Christian Ledesma <christian.ledesma@neoris.com>
2022-06-02 17:05:10 +0200

    Setup sqlite with entity framework

--- LExpress.Api/Program.cs
+++ LExpress.Api/Program.cs
@@ -1,8 +1,18 @@
+using LExpress.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(ops =>
+    {
+        builder.Configuration.GetConnectionString("DefaultConnection");
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
