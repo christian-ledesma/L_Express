aecdb60aef1f7ac59377feccea7c44e5eace7382
Christian <christian_ledesma_a@hotmail.com>
2022-09-11 01:27:15 +0200

    Refactor and create shop module

--- LExpress.Web/src/app/app.component.ts
+++ LExpress.Web/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/Pagination';
-import { IProduct } from './models/Product';
+import { IPagination } from './shared/models/Pagination';
+import { IProduct } from './shared/models/Product';
 
 @Component({
   selector: 'app-root',
